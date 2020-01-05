3ad99885a1b652950d7c54527be8ca05d4832884
kabeh2 <mattcnk@hotmail.com>
2020-01-05 16:29:49 -0500

    Updates

---
+++ precache-manifest.21a55372f124313f007e0d6a825aa70b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a18efce35c6b9a9bedd4b42f53fca3",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "b52c3917d58c150173bc",
+    "url": "/react-redux-app/static/css/main.567482e3.chunk.css"
+  },
+  {
+    "revision": "36e0f64a93516c31d54e",
+    "url": "/react-redux-app/static/js/2.ece6c876.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/react-redux-app/static/js/2.ece6c876.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b52c3917d58c150173bc",
+    "url": "/react-redux-app/static/js/main.3a2b4c23.chunk.js"
+  },
+  {
+    "revision": "d6695a0fe15779c1881e",
+    "url": "/react-redux-app/static/js/runtime-main.0dc654e2.js"
+  }
+]);
